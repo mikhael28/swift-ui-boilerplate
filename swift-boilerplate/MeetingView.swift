f64f07a156eb925315443ecdd27c19246972d282
mikhael28 <mikhael@hey.com>
2021-06-17 08:32:26 -0700

    initial commit

---
+++ swift-boilerplate/MeetingView.swift
@@ -0,0 +1,25 @@
+import SwiftUI
+
+struct MeetingView: View {
+    var body: some View {
+        VStack {
+            ProgressView(value: 5.0, total: 15)
+            HStack {
+                VStack {
+                    Text("Seconds Elapsed")
+                    Label("300", systemImage: "hourglass.bottomhalf.fill")
+                }
+                VStack {
+                    Text("Seconds Remaining");
+                    Label("600", systemImage: "hourglass.tophalf.fill")
+                }
+            }
+        }
+    }
+}
+
+struct MeetingView_Previews: PreviewProvider {
+    static var previews: some View {
+        MeetingView()
+    }
+}
---
+++ swift-boilerplate/swift_boilerplateApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_boilerplateApp.swift
+//  swift-boilerplate
+//
+//  Created by Mikha'el on 6/17/21.
+//
+
+import SwiftUI
+
+@main
+struct swift_boilerplateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MeetingView()
+        }
+    }
+}
